0f807dd3942ab55a9b8b43e6c1c246f7756efd4f
LeftCircle <rcato60@gmail.com>
2024-05-21 13:08:06 -0400

    Added component array to entities

--- Scenes/Entity/Entity.gd
+++ Scenes/Entity/Entity.gd
@@ -1,7 +1,16 @@
 extends Node
 class_name Entity
 
+@export var starting_components : Array[Component] = []
+
 var entity_id : int = EntityManager.NULL_ID
 
 func _init() -> void:
 	entity_id = EcsCoordinator.create_entity()
+
+func _ready() -> void:
+	for comp in starting_components:
+		EcsCoordinator.add_component(entity_id, comp)
+
+func _exit_tree() -> void:
+	EcsCoordinator.destroy_entity(entity_id)
